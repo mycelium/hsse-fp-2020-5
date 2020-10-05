3c9b7f55fc92659ecfab7ff95fbd5547d0f4de30
Vladkuz <vladkuz100@mail.ru>
2020-10-05 22:34:46 +0300

    lab 3 implemented by VK
    lab 3 implemented by VK

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+split(X,[],[],[]).
+split(X,[Y|Remainder],[Y|Smaller],Bigger):- X>Y, split(X,Remainder,Smaller,Bigger).
+split(X,[Y|Remainder],Smaller,[Y|Bigger]):- split(X,Remainder,Smaller,Bigger).
+
+qsort([],[]).
+qsort([L|Remainder],K) :-
+    split(L,Remainder,Smaller,Bigger),
+    qsort(Smaller,KSmaller),
+    qsort(Bigger,KBigger),
+    append(KSmaller,[L|KBigger],K).
+
+% ?- qsort([24,3,8,5],X).
+% X = [3, 5, 8, 24] .
+
+% ?- qsort([75,10,13],X).
+% X = [10, 13, 75]
