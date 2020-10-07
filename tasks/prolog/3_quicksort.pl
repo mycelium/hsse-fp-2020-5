0294a7a803d63aa8513b16e57c428deafaeb4877
Konstantin <derpomorj@gmail.com>
2020-10-07 22:12:23 +0300

    add 3rd prolog lab

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort(L, K):- qsort(L, K, []). 
+qsort([], K, K).
+qsort([X|L], K, Z):-
+  partition(L, X, M, N),
+  qsort(N, W, Z),
+  qsort(M, K, [X|W]).
+  
+partition([K|L], X, M, [K|N]):-
+  X < K, !,
+  partition(L, X, M, N).
+  
+partition([K|L], X, [K|M], N):-
+  partition(L, X, M, N).
+  
+partition([], _, [], []).
