0aab023c823e47996e25926162cd3476ee7136b2
sophiacrumb <62599270+sophiacrumb@users.noreply.github.com>
2020-10-12 20:14:08 +0300

    Added code to first scala task

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,22 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c ==  0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+   def indexPar(chars: List[Char], parBalance: Int): Int = {
+      if (chars.isEmpty || parBalance < 0) parBalance
+      else if (chars.head == ')') indexPar(chars.tail, parBalance - 1)
+      else if (chars.head == '(') indexPar(chars.tail, parBalance + 1)
+      else indexPar(chars.tail, parBalance)
+   }
+               
+   indexPar(expressionChars, 0) == 0
   }
 
   /**
@@ -33,6 +41,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    def actualChange(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else actualChange(money, coins.tail) + actualChange(money - coins.head, coins)
+    }
+    
+    actualChange(money, coins)
   }
 }
