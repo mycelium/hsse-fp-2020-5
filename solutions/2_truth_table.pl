238b312d9e43f30f140586e7bc08d6a9ed174bb1
Spectra456 <spectranoid123@gmail.com>
2020-10-12 21:13:15 +0300

    added 2 lab

---
+++ solutions/2_truth_table.pl
@@ -0,0 +1,54 @@
+% определить предикаты:
+	% and(A,B)
+	% or(A, B)
+	% xor(A, B)
+	% not(A)
+	% equ(A,B)
+  
+% ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
+% truth_table(A,B,and(A,or(A,B))).
+% true true true
+% true fail true
+% fail true fail
+% fail fail fail
+
+bool(true).
+bool(false).
+
+not(A) :-
+  + A.
+
+and(A, B) :-
+	A,
+	B.
+
+or(A, B) :-
+	not(and(not(A),	not(B))).
+
+xor(A, B) :-
+	or(and(A, not(B)), and(not(A), B)).
+
+equ(A, B) :-
+	not(xor(A, B)).
+
+evaluation(Expression, true) :- Expression, !.
+evaluation(Expression, false) :- not(Expression), !.
+
+truth_table(A, B, Expression) :-
+	bool(A), 
+	bool(B),
+	evaluation(Expression, Res),
+	write(A), 
+	write('\t'), 
+	write(B), 
+	write('\t'),
+	write(Res),
+	nl, fail.
+
+
+% ?- truth_table(A, B, and(xor(A, B), or(A, B))).
+% true    true    false
+% true    false   true
+% false   true    true
+% false   false   false
+% false.
