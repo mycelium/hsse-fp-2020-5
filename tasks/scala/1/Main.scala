13b20f209c2bead7c670a3bd2a27d03620ddfc69
Egor Shakhmin <lapter57@gmail.com>
2020-10-14 23:06:45 +0300

    Scala-1 was done

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,6 @@
 package recfun
-import common._
+
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -15,24 +16,37 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) {
+      return 1
+    }
+    pascal(c-1, r-1) + pascal(c, r-1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    @tailrec
+    def balanceWithCount(chars: List[Char], count: Int): Boolean = (chars, count) match {
+      case (_, cnt) if cnt < 0 => false
+      case (Nil, cnt) => cnt == 0
+      case ('(' :: tail, cnt) => balanceWithCount(tail, cnt + 1)
+      case (')' :: tail, cnt) => balanceWithCount(tail, cnt - 1)
+      case (_ :: tail, cnt) => balanceWithCount(tail, cnt)
+    }
+    balanceWithCount(chars, 0)
   }
 
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
    * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
+   * there is 1 way to give change for 5 if you have coins with denomination
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int = (money, coins) match {
+    case (rest, _) if rest == 0 => 1
+    case (rest, cash) if rest < 0 || cash.isEmpty => 0
+    case _ => countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
