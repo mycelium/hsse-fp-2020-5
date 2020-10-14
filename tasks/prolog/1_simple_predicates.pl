59f9fac8eaa533110d230bb8313c4a5d55789d57
Andrew Petrov <marsofandrew@gmail.com>
2020-10-14 22:53:25 +0300

    Implemented lab 4 and move all labs to folder tasks

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -5,13 +5,21 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
 	?- brother(X,Y).
 	?- cousin(X,Y).
 	?- grandson(X,Y).
 	?- descendent(X,Y).
+
+
+father(a, b).
+father(a, c).
+father(b, d).
+father(b, e).
+father(c, f).
+
+brother(X, Y) :- father(F,Y), father(F,X), X \= Y.
+cousin(X, Y) :- father(F1, X), father(F2, Y), brother(F1, F2), X \= Y, F1 \= F2.
+grandson(X, Y) :- father(F, X), father(Y, F).
+descendent(X,Y) :- father(Y, X).
+descendent(X,Y) :- father(Z, X), descendent(Z, Y).
--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,24 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:-['3_quicksort'].
+
+balanced_tree(L,T) :- qsort(L, S), create_tree(S, T).
+
+create_tree([], empty).
+create_tree(S, instant(ROOT, L, R)) :- divide(S, LEFT, [ROOT | RIGHT]), create_tree(LEFT, L), create_tree(RIGHT, R).
+
+divide(S, L, R) :-  length(S, LEN), RLEN is LEN - LEN // 2, length(R, RLEN), append(L, R, S).
+
+'
+?- create_tree([0, 1, 2], T).
+T = instant(1, instant(0, empty, empty), instant(2, empty, empty)) ;
+false.
+
+?- balanced_tree([25, 14, -5, 18, 85, 2, 0, 12, 15], T).
+T = instant(14, instant(2, instant(0, instant(-5, empty, empty), empty), instant(12, empty, empty)), instant(25, instant(18, instant(15, empty, empty), empty), instant(85, empty, empty))) ;
+false.
+
+
+'
