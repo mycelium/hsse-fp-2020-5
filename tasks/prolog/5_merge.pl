f3e2f3dc2f238e4aef174756de48ca43da2220e3
Kirill Stonozhenko <kirill_stonozhenko@dell.com>
2020-10-15 13:43:54 +0300

    Fifth lab added

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,19 @@
 $ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
+
+:- consult('3_quicksort.pl').
+
+sorted([]).
+sorted([L]).
+sorted([X,Y|Tail]) :-
+    order(X,Y),
+    sorted([Y|Tail]).
+
+order(X, Y) :-
+    X =< Y.
+
+mrg(L1, L2, R) :-
+    sorted(L1),
+    sorted(L2),
+    append(L1, L2, Unsorted),
+    qsort(Unsorted, R).
