fb364827bf5f514d5f594c7475fa2fd298744940
m.shkividorov <maksimshkiv@mail.ru>
2020-10-15 16:14:46 +0300

    the first three lab work done

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -10,8 +10,76 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+	brother(X, Y) :- father(Z, X), father(Z, Y), X \= Y.
+
+	cousin(X, Y) :-
+		father(Ded, Dad_X),
+		father(Ded, Dad_Y),
+		father(Dad_X, X),
+		father(Dad_Y, Y),
+		X \= Y,
+		Dad_X \= Dad_Y.
+
+	grandson(X, Y) :- father(X, Z), father(Z, Y).
+
+	descendent(X, Y) :-
+		father(X, Y).
+
+	descendent(X, Y) :- father(X, Z),descendent(Z, Y).
+
+	% указать в каком порядке и какие ответы генерируются вашими методами
+	%	?- brother(X,Y).
+	%	?- cousin(X,Y).
+	%	?- grandson(X,Y).
+	%	?- descendent(X,Y).
+
+	% ?- brother(X, Y).
+	% X = b,
+	% Y = c ;
+	% X = c,
+	% Y = b ;
+	% X = d,
+	% Y = e ;
+	% X = e,
+	% Y = d ;
+	% false.
+
+	% ?- cousin(X, Y).
+	% X = d,
+	% Y = f ;
+	% X = e,
+	% Y = f ;
+	% X = f,
+	% Y = d ;
+	% X = f,
+	% Y = e ;
+	% false.
+
+	% ?- grandson(X, Y).
+	% X = a,
+	% Y = d ;
+	% X = a,
+	% Y = e ;
+	% X = a,
+	% Y = f ;
+	% false.
+
+	% ?- descendent(X, Y).
+	% X = a,
+	% Y = b ;
+	% X = a,
+	% Y = c ;
+	% X = b,
+	% Y = d ;
+	% X = b,
+	% Y = e ;
+	% X = c,
+	% Y = f ;
+	% X = a,
+	% Y = d ;
+	% X = a,
+	% Y = e ;
+	% X = a,
+	% Y = f ;
+	% false.
--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+split(X,[],[],[]).
+split(X,[Y|Remember],[Y|Little],Larger):- X>Y, split(X,Remember,Little,Larger).
+split(X,[Y|Remember],Little,[Y|Larger]):- split(X,Remember,Little,Larger).
+
+qsort([],[]).qsort([L|Remember],K) :-
+split(L,Remember,Little,Larger),
+    qsort(Little,KSmaller),
+    qsort(Larger,KBigger),
+    append(KLittle,[L|KLarger],K).
+
+$ ?- qsort([6,2,0,1,-5], K).
+% K = [-5, 0, 1, 2, 6] .
+	
