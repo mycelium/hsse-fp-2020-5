0711b778182c1139da586b9e7758d89a0461a53c
Kirill Stonozhenko <kirill_stonozhenko@dell.com>
2020-10-15 13:17:28 +0300

    Forth lab added

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,21 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult('3_quicksort.pl').
+
+balanced_tree(List, Tree) :-
+    qsort(List,Sorted),
+    create_node(Sorted,Tree).
+
+create_node([], empty).
+create_node(List, instant(Key, LeftTree, RightTree)) :-
+    mediana(List, LeftList, [Key | RightList]),
+    create_node(LeftList, LeftTree),
+    create_node(RightList, RightTree).
+
+mediana(List, LeftList, RightList) :-
+    length(List, Len),
+    MedLen is Len // 2,
+    length(LeftList, MedLen),
+    append(LeftList, RightList, List).
