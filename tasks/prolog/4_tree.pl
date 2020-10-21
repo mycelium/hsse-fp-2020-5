8bf015efffbe10aa7587483636a2ba55006e766b
Philipp <Crisis4433@gmail.com>
2020-10-21 22:27:29 +0300

    Added 4 prolog lab

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -1,5 +1,17 @@
 % используя предикат qsort(L,K) из предыдущего задания разработать предикат:
 % balanced_tree(L,T) - который по заданном списку строит сбалансированное бинарное дерево поиска
 % для построения дерева использовать следующие  нотации:
-% empty - пустое дерево 
+% empty - пустое дерево
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult('3_quicksort.pl').
+
+balanced_tree(L, T) :- qsort(L, SorD),
+	balanced(SorD, T).
+balanced([], empty).
+balanced(L, instant(R, BeneathL, BeneathR)) :- splittree(L, LTree, [R|RTree]),
+	balanced(LTree, BeneathL), balanced(RTree, BeneathR).
+
+splittree(L, LTree, RTree) :- length(L, LOfTree),
+	Mid is div(LOfTree, 2), length(LTree, Mid),
+	append(LTree, RTree, L).
