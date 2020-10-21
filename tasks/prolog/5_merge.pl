31a6020be0db69d66508d49503c9bc8ac9c3122d
Philipp <Crisis4433@gmail.com>
2020-10-21 22:30:51 +0300

    Added 5 prolog lab

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,14 @@
-$ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
+$ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2
 % определяет список R, составленный из этих элементов
+
+mrg([Arr | ListOne], [Brr | ListTwo], [Arr | Res]) :- Arr =< Brr,
+    mrg(ListOne, [Brr | ListTwo], Res).
+
+mrg([Arr | ListOne], [Brr | ListTwo], [Brr | Res]) :- Arr > Brr,
+    mrg([Arr | ListOne], ListTwo, Res).
+
+mrg([], [Brr | ListTwo], [Brr | Res]) :- mrg([], ListTwo, Res).
+
+mrg([Arr | ListOne], [], [Arr | Res]):-  mrg(ListOne, [], Res).
+
+mrg([],[],[]).
