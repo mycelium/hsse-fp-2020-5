c9a9b71feeb846cf413256c8d299b4d8cc2a807b
Nikita Makarov <makar-26.05.98@mail.ru>
2020-10-21 22:15:50 +0300

    [NEW]: add 4, 5 labs for Prolog.

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,27 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+pivot(_, [], [], []).
+pivot(Pivot, [L|Tail], [L|LessOrEqualThan], GreaterThan) :- Pivot >= L, pivot(Pivot, Tail, LessOrEqualThan, GreaterThan). 
+pivot(Pivot, [L|Tail], LessOrEqualThan, [L|GreaterThan]) :- pivot(Pivot, Tail, LessOrEqualThan, GreaterThan).
+
+qsort([], []).
+qsort([L|Tail], K) :- pivot(L, Tail, List1, List2), qsort(List1, KList1), qsort(List2, KList2), append(KList1, [L|KList2], K).
+
+split(L, Left, Right):- 
+    append(Left, Right, L),
+    length(L, LeftSize),
+    Mid is div(LeftSize, 2),
+    length(Left, Mid).
+
+balanced_tree([], empty).
+
+balanced_tree(L, instant(Root, LeftSub, RightSub)) :- 
+    split(L, Left, [Root|Right]),
+    balanced_tree(Left, LeftSub),
+    balanced_tree(Right, RightSub).
+
+balanced_tree(L, T) :- 
+    qsort(L, S),
+    balanced_tree(S, T).
