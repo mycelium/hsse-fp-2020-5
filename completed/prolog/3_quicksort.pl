1ac5e848ce0ebe7610e0a7945214688f627e277f
Voland28 <d_roman.kst@mail.ru>
2020-10-27 00:44:49 +0500

    Added lab 1,2,3

---
+++ completed/prolog/3_quicksort.pl
@@ -0,0 +1,15 @@
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+apnd([], X, X).
+apnd([X|Y],Z,[X|W]) :- apnd(Y,Z,W).
+qsort([],[]).
+qsort([H|T],R):-
+    pivoting(H,T,L1,L2),qsort(L1,R1),qsort(L2,R2),apnd(R1,[H|R2],R).
+
+pivoting(H,[],[],[]).
+pivoting(H,[X|T],[X|L],G):-X=<H,split(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]):-X>H,split(H,T,L,G).
+
+%| ?- qsort( [4,9,1,3,5],C).
+%
+%C = [1,3,4,5,9] ? ;
