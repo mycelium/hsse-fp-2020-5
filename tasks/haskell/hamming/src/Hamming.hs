72d71af7eb4cd805ae3b1a123e51c884593bd401
Oleg Kuzmin <Oleg.Kuzmin@emc.com>
2020-10-27 15:58:49 +0300

    Fix haskell solution

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -4,17 +4,8 @@ import Data.Maybe
 
 distance :: String -> String -> Maybe Int
 distance xs ys =
-    if ((length xs) > 0 || (length ys) > 0)
+    if ((length xs) == (length ys))
         then
-            if ((length xs) > 0 && (length ys) > 0)
-                then
-                   if (isJust (distance (drop 1 xs) (drop 1 ys)))
-                       then
-                           Just ((fromJust (distance (drop 1 xs) (drop 1 ys))) + (if (head xs) /= (head ys) then 1 else 0))
-                       else
-                           Nothing
-                else
-                    Nothing
+            Just (sum (map fromEnum (map (uncurry (/=)) (zip xs ys))))
         else
-            Just 0
-
+            Nothing
