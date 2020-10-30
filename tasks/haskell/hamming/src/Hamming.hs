56c3f2a74ca0168147af9f9c3e05c59c1dee56ff
mlitvinov <46522074+mlitvinov@users.noreply.github.com>
2020-10-30 22:03:10 +0300

    Add files via upload

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,17 @@
 module Hamming (distance) where
 
+--declare function distance and describe its args
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+--if strings have different lengths -> return Nothing (can't evaluate Hamming distance)
+--otherwise (any else aren't true) we start counting Hamming distance from 0
+distance xs ys  | (length xs) /= (length ys) = Nothing | otherwise = dist 0 xs ys                
+  where
+--starting implementation -> little bit kind of Prolog algs =)
+--returning when there are empty lists left
+    dist counter [] [] = Just counter
+--take elem from every string
+    dist counter (x:xs) (y:ys) = 
+--checking these elements for equality
+      if x /= y
+        then dist (counter+1) xs ys
+      else dist counter xs ys
