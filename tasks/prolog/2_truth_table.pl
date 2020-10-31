e3fe262980c458113f0f6ebc48540ac9c4345c19
Alexander Aphonin <alex15051997@yandex.ru>
2020-10-31 18:28:53 +0300

    Add logical predicates

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,9 +1,20 @@
 % определить предикаты:
 	% and(A,B)
+and(A,B) :- A, B.
+
 	% or(A, B)
-	% xor(A, B)
-	% not(A)
+or(A,B) :- A; B.
+
 	% equ(A,B)
+equ(A,B) :- A = B.
+    
+	% xor(A, B)
+xor(A, B) :- not(equ(A,B)).
+
+	% not(A)
+%already defined. built-in.
+
+
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
