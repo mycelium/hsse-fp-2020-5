d7a1a6ba429e0b69422ab1a11c41e77dee26d5c3
Galina <37181591+shandakova@users.noreply.github.com>
2020-11-01 21:53:48 +0300

    Add fourth prolog lab

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,25 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult('3_quicksort.pl').
+
+balanced_tree(L, T) :-
+	qsort(L, Sort),
+	make_balanced_tree(Sort, T).
+
+split_middle(X, L, R):-
+	length(X, Len),
+	Mid is Len // 2,
+	length(L, Mid),
+	append(L, R, X).
+
+make_balanced_tree([], empty).
+make_balanced_tree(List, instant(Root, L_tree, R_tree)) :-
+	split_middle(List, L, [Root|R]),
+	balanced_tree(L, L_tree),
+	balanced_tree(R, R_tree).
+
+
+% ?-balanced_tree([5, 1, -5, 8, 0], T).
+% T = instant(1, instant(0, instant(-5, empty, empty), empty), instant(8, instant(5, empty, empty), empty))
