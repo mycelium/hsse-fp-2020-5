90f337ec66cc68b8c62b2829c39bc1d534503d65
Galina <37181591+shandakova@users.noreply.github.com>
2020-11-01 21:40:22 +0300

    Add third prolog lab

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,20 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([],[]).
+qsort([Current | Rest], Sort) :-
+    partition(Current, Rest, LList, RList),
+    qsort(LList, LSort),
+    qsort(RList, RSort),
+    append(LSort, [Current | RSort], Sort).
+
+partition(_, [], [], []).
+partition(Current, [Num | Rest], [Num | Left], Right) :-
+    Num < Current,
+    partition(Current, Rest, Left, Right).
+
+partition(Current, [Num | Rest], Left, [Num |Right]) :-
+    Num >= Current,
+    partition(Current, Rest, Left, Right).
+
+% ?-qsort([2,3,-5,55,666,0,-4],List).
+% List = [-5, -4, 0, 2, 3, 55, 666]
