bc349a13a972083ea5733b231dadf50efb885be6
Соколова Ольга Алексеевна <Olga.Sokolova@bercut.com>
2020-11-04 17:47:49 +0300

    Add scala lab2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,6 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(map(singletonSet(92), (x:Int) => x + 3))
+  print(forall(singletonSet(92), (x:Int) => x == 92))
 }
