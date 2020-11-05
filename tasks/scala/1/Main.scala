e5ba891f4bd8477b2ee5e098599a3811faeb0ae2
agaev.aslan <agaev.aslan@gmail.com>
2020-11-05 18:58:59 +0300

    lab1 scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +8,42 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("\n\n ========= 2 ========")
+    println(balance("case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)".toList))
+    println(balance("def count(chars: List[Char], counter: Int): Boolean = {)))((".toList))
+    println(balance(")))()()()()()()()()))".toList))
+
+    val money = 4
+    val coins_ = List(1, 2, 6)
+    println("\n\n ========= 3 ========")
+    println(s"money: $money, coins_: $coins_")
+    println(s"countChange: ${countChange(money, coins_)}")
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-
+  def pascal(c: Int, r: Int): Int = c match {
+    case 0 => 1
+    case `r` => 1
+    case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(chars: List[Char], counter: Int): Boolean = {
+      if (chars.isEmpty) return counter == 0
+      chars.head match {
+        case '(' => count(chars.tail, counter + 1)
+        case ')' => count(chars.tail, counter - 1)
+        case _ => count(chars.tail, counter)
+      }
+    }
+
+    count(chars, 0)
   }
 
   /**
@@ -32,7 +53,13 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
+  def countChange(money: Int, coins: List[Int]): Int = coins match {
+    case _ if money == 0 => 1
+    case Nil if money != 0 => 0
+    case coin :: tail =>
+      if (money - coin < 0) countChange(money, tail)
+      else countChange(money - coin, coins) + countChange(money, tail)
 
   }
+
 }
