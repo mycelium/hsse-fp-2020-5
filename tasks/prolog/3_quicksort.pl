5363ca6b59fad06e0451c210fbaab20e43bd19b6
Angelina Barkanova <71984597+gelichan@users.noreply.github.com>
2020-11-07 21:12:05 +0300

    Added solution for prolog task3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,12 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+max(X,Y) :- X > Y.
+
+pivot(_,[],[],[]).
+pivot(X,[Y|T],[Y|Min],Max) :- max(X,Y),!,pivot(X,T,Min,Max).
+pivot(X,[Y|T],Min,[Y|Max]) :- pivot(X,T,Min,Max).
+
+qsort([],[]).
+qsort([X|T],List) :- pivot(X,T,Min,Max),qsort(Min,SortedMin),qsort(Max,SortedMax),append(SortedMin,[X|SortedMax],List).
+
+% ?- qsort([43,-10,10,1,32,-10,-23,84,279],K).
+% K = [-23, -10, -10, 1, 10, 32, 43, 84, 279]
