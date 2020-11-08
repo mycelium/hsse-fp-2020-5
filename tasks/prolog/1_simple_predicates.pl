3797ebbd3a1f479e159e28d2bffe2e6a62249ee0
selivi <Ivan.Selivanov@dell.com>
2020-11-08 20:59:12 +0300

    Add prolog and scala labs

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,3 +1,4 @@
+  
 % есть набор фактов вида father(person1, person2) (person1 is the father of person2)
 % Необходимо определить набор предикатов:
 % 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
@@ -5,13 +6,64 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+
+father(a,b).  % 1                 
+father(a,c).  % 2
+father(b,d).  % 3
+father(b,e).  % 4
+father(c,f).  % 5
+brother(X,Y) :- father(Z,X),father(Z,Y),X \= Y.
+cousin(X,Y) :- father(A,X),father(B,Y),brother(A,B).
+grandson(X,Y) :- father(X,Z),father(Z,Y).
+descendent(X,Y) :- father(X,Y);father(X,Z),descendent(Z,Y).
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%?- brother(X,Y).
+%X = b,
+%Y = c ;
+%X = c,
+%Y = b ;
+%X = d,
+%Y = e ;
+%X = e,
+%Y = d ;
+%false.
+
+%?- cousin(X,Y).
+%X = d,
+%Y = f ;
+%X = e,
+%Y = f ;
+%X = f,
+%Y = d ;
+%X = f,
+%Y = e ;
+%false.
+
+%?- grandson(X,Y).
+%X = a,
+%Y = d ;
+%X = a,
+%Y = e ;
+%X = a,
+%Y = f ;
+%false.
+
+%?- descendent(X,Y).
+%X = a,
+%Y = b ;
+%X = a,
+%Y = c ;
+%X = b,
+%Y = d ;
+%X = b,
+%Y = e ;
+%X = c,
+%Y = f ;
+%X = a,
+%Y = d ;
+%X = a,
+%Y = e ;
+%X = a,
+%Y = f ;
+%false.
--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,14 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+quicksort([],[]).   
+quicksort([X|Tail], Sorted) :-         
+   split(X, Tail, Small, Big),
+    quicksort(Small, SortedSmall),
+    quicksort(Big, SortedBig),
+    append(SortedSmall, [X|SortedBig], Sorted).
+
+split(X, [],[],[]).
+split(X, [Y|Tail], [Y|Small], Big) :-
+    X > Y,
+    split(X, Tail, Small, Big).
+split(X, [Y|Tail], Small, [Y|Big]) :-
+    split(X, Tail, Small, Big).
+	
