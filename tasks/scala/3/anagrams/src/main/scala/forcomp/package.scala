43a5e3314a3e1ab9d5b51fe523fa872247386049
Philipp <Crisis4433@gmail.com>
2020-11-08 23:08:29 +0300

    Added 3 scala

--- tasks/scala/3/anagrams/src/main/scala/forcomp/package.scala
+++ tasks/scala/3/anagrams/src/main/scala/forcomp/package.scala
@@ -1,11 +1,25 @@
+import java.io.File
+
 package object forcomp {
   val dictionaryPath = List("forcomp", "linuxwords.txt")
+
+  def subFile(file: File, children: String*) = {
+    children.foldLeft(file)((file, child) => new File(file, child))
+  }
+
+  def resourceAsStreamFromSrc(resourcePath: List[String]): Option[java.io.InputStream] = {
+    val classesDir = new File(getClass.getResource(".").toURI)
+    val projectDir = classesDir.getParentFile.getParentFile.getParentFile.getParentFile
+    val resourceFile = subFile(projectDir, ("src" :: "main" :: "resources" :: resourcePath): _*)
+
+    if (resourceFile.exists) Some(new java.io.FileInputStream(resourceFile)) else None
+  }
 
   def loadDictionary = {
     val wordstream = Option {
       getClass.getClassLoader.getResourceAsStream(dictionaryPath.mkString("/"))
     } orElse {
-      common.resourceAsStreamFromSrc(dictionaryPath)
+      resourceAsStreamFromSrc(dictionaryPath)
     } getOrElse {
       sys.error("Could not load word list, dictionary file not found")
     }
