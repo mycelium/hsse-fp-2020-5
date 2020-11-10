301b37787853ca4ebd3b423c6194b238022446b2
Maxim Shkividorov <32208852+juseppeinside@users.noreply.github.com>
2020-11-10 21:41:25 +0300

    Update Main.scala
    add 1th lab scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,38 +1,55 @@
 package recfun
-import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
+    println("Pascal Delta")
     for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
+      for (col <- 0 to row) {print(pascal(col, row) + " ")}
+      println();
     }
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-
+  
+  def pascal(c_d: Int, r_d: Int): Int = {
+    if (c_d== 0 || c_d== r_d) {
+      return 1
+    }
+    pascal(c_d-1, r_d-1) + pascal(c_d, r_d-1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    @tailrec
+    def balanceWithCount(chars: List[Char], count: Int): Boolean = (chars, count) match {
+      case (_, c_nt) if c_nt < 0 => false
+      case (Nil, c_nt) => c_nt == 0
+      case ('(' :: tail, c_nt) => balanceWithCount(tail, c_nt + 1)
+      case (')' :: tail, c_nt) => balanceWithCount(tail, c_nt - 1)
+      case (_ :: tail, c_nt) => balanceWithCount(tail, c_nt)
+    }
+    balanceWithCount(chars, 0)
   }
 
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
    * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
+   * there is 1 way to give change for 5 if you have coins with denomination
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  
+  def countChange(money: Int, coins: List[Int]): Int = (money, coins) match {
+    case 
+      (rest, _) if rest == 0 => 1
+    case 
+      (rest, cash) if rest < 0 || cash.isEmpty => 0
+    case _ => 
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
