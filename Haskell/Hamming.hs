0d36eb90e8b668be6fedd940f8489e8667071b13
sventy45 <51117681+sventy45@users.noreply.github.com>
2020-11-10 11:08:08 +0300

    add haskell solution

--- Haskell/Hamming.hs
+++ Haskell/Hamming.hs
@@ -5,6 +5,5 @@ distance [] [] = Just 0
 distance (x:xs) (y:ys) = if length xs /= length ys then 
 Nothing else Just (counter (x:xs) (y:ys))
                          where counter [] [] = 0
-                               counter (x:xs) (y:ys) = if x /= y 
-                               then (+) 1 ((counter) xs ys) 
+                               counter (x:xs) (y:ys) = if x /= y  then (+) 1 ((counter) xs ys) 
                                else (counter) xs ys
