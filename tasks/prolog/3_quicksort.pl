b38aa1cc87f40b8656fb3854323f7c5ac224c11b
AlexandraSokolova <37152165+AlexandraSokolova@users.noreply.github.com>
2020-11-12 15:05:58 +0300

    Added third prolog lab

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,21 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([],[]).
+qsort(L,K):-
+		L = [Head|Tail],
+		partition(Head, Tail, L1, L2),
+		qsort(L1, Sort1),
+		qsort(L2, Sort2),
+		append(Sort1, [Head|Sort2], K).
+
+partition(_, [], [], []).
+partition(N, [Head|Tail], [Head|Left], Right) :-
+    Head =< N,
+    partition(N, Tail, Left, Right).
+
+partition(N, [Head|Tail], Left, [Head|Right]) :-
+    Head > N,
+    partition(N, Tail, Left, Right).
+	
+% ?- qsort([-200,0,1746,1,42,-30,98],Result).
+% Result = [-200, -30, 0, 1, 42, 98, 1746] 
