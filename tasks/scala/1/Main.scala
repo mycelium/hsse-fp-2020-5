f6e46dec79f44b23b2682c058befc1a468efc1d7
pavel <ghty98@gmail.com>
2020-11-16 21:29:47 +0300

    add scala labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,47 @@
 package recfun
-import common._
+//import common._
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+    println(pascal(1, 3))
+    println(balance("123".toList))
+    println(balance("((123)".toList))
+    println(balance("((12)3)".toList))
+    println(countChange(5, List(1)))
+    println(countChange(4, List[Int](1, 2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+    def nextRow(l: List[Int]): List[Int] = l match {
+      case a :: b :: t => (a + b) :: nextRow(b :: t)
+      case _ => l
+    }
 
+    @scala.annotation.tailrec
+    def getRow(r: Int, l: List[Int]): List[Int] = r match {
+      case r if r > 0 => getRow(r - 1, nextRow(1 :: l))
+      case 0 => 1 :: l
+      case _ => throw new IllegalArgumentException("r must be >= 0")
+    }
+
+    getRow(r, Nil)(c)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def difference(chars: List[Char]): Int = chars match {
+      case '(' :: t => difference(t) + 1
+      case ')' :: t => difference(t) - 1
+      case _ :: e => difference(e)
+      case Nil => 0
+    }
+
+    difference(chars) == 0
   }
 
   /**
@@ -32,7 +51,10 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int = (money, coins) match {
+    case (remainder, _) if remainder < 0 => 0
+    case (_, Nil) => 0
+    case (0, _) => 1
+    case (remainder, h :: t) => countChange(remainder - h, h :: t) + countChange(remainder, t)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -1,6 +1,52 @@
 package funsets
 
 object Main extends App {
+
   import FunSets._
+
+  print("Set {1} contain 1: ")
   println(contains(singletonSet(1), 1))
+
+  print("Union of {1} and {2} is: ")
+  val setUnion: Set = union(singletonSet(1), singletonSet(2))
+  printSet(setUnion)
+
+  print("Intersect of {1,2} and {2} is: ")
+  val setIntersect: Set = intersect(setUnion, singletonSet(2))
+  printSet(setIntersect)
+
+  print("Diff of {1,2} and {2} is: ")
+  val setDiff: Set = diff(setUnion, setIntersect)
+  printSet(setDiff)
+
+  print("Filter of {1,2} and {1} is: ")
+  val setFilter: Set = filter(setUnion, setDiff)
+  printSet(setFilter)
+
+  print("All bounded of {1,2,3,-1001,1000} satisfy {1,2} is: ")
+  println(
+    forall(union(union(union(
+      setUnion,
+      singletonSet(3)),
+      singletonSet(-1001)),
+      singletonSet(2000)), setUnion))
+
+  print("All bounded of {1,2,-1001,1000} satisfy {1,2} is: ")
+  println(
+    forall(union(union(
+      setUnion,
+      singletonSet(-1001)),
+      singletonSet(2000)), setUnion))
+
+  print("Exist bounded of {1,2,3,-1001,1000} satisfy {1,2} is: ")
+  println(
+    exists(union(union(union(
+      setUnion,
+      singletonSet(3)),
+      singletonSet(-1001)),
+      singletonSet(2000)), setUnion))
+
+  print("Multiply all elements in {1,2} by 2: ")
+  val setMap: Set = map(setUnion, value => value * 2)
+  printSet(setMap)
 }
