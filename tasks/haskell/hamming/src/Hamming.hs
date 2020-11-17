1acf24bc3201f041951b8fc9fd14ffb1a0d6b735
Ilya Malov <IlyamalovCES@gmail.com>
2020-11-17 21:39:01 +0300

    added haskell

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,10 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = accumulator 0 xs ys
+  where accumulator sum (x:xs) (y:ys) = if x == y
+                                        then accumulator sum xs ys
+                                        else accumulator (sum + 1) xs ys
+        accumulator sum [] [] = Just sum
+        accumulator _ [] _ = Nothing
+        accumulator _ _ [] = Nothing
