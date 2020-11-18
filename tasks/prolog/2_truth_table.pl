4c722c6571175088881b031b837fa646c1f8f0d1
Yaros554 <yaroslavopar@yandex.ru>
2020-11-18 17:48:29 +0300

    Lab 1 and Lab 2 Prolog

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,38 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+
+or(A, B) :- A; B.
+
+not(A) :- A, !, fail.
+not(A).
+
+xor(A, B) :- or(A,B), not(and(A, B)).
+
+equ(A,B) :- A = B.
+
+ab_value(true).
+ab_value(fail).
+
+expr(E, true) :- E, !.
+expr(E, fail).
+
+truth_table(A,B,Expression) :-
+  ab_value(A),
+  ab_value(B),
+  write(A),
+  write(' '),
+  write(B),
+  write(' '),
+  expr(Expression, Res),
+  write(Res),
+  nl, 
+  fail.
+  
+%?- truth_table(A,B,and(A,or(A,B))).
+%true	true	true
+%true	fail	true
+%fail	true	fail
+%fail	fail	fail
