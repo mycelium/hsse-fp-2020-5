8160606148a7be39f7e8e4ff843106befca95489
Yaros554 <yaroslavopar@yandex.ru>
2020-11-18 23:38:41 +0300

    Update 3_quicksort.pl
    Lab3 Prolog

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+roman_empire([], SuppElem, [], []):-!.
+roman_empire([Head|Tail], SuppElem, [Head|BigList], SmallList):-
+  Head >= SuppElem, !, roman_empire(Tail, SuppElem, BigList, SmallList).
+roman_empire([Head|Tail], SuppElem, BigList, [Head|SmallList]):-
+  roman_empire(Tail, SuppElem, BigList, SmallList).
+  
+qsort([], []).
+qsort([Elem], [Elem]).
+qsort([SuppElem|Tail], ListSort):-
+  roman_empire(Tail, SuppElem, BigList, SmallList),
+  qsort(BigList, BigListSort),
+  qsort(SmallList, SmallListSort),!,
+  append(SmallListSort, [SuppElem|BigListSort], ListSort).
