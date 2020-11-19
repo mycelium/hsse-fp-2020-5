d96a38040f030b3e084a918aad6754fe4199204b
Yaros554 <yaroslavopar@yandex.ru>
2020-11-19 20:59:28 +0300

    Update 4_tree.pl
    Lab 4 Prolog

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,33 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+roman_empire([], SuppElem, [], []):-!.
+roman_empire([Head|Tail], SuppElem, [Head|BigList], SmallList):-
+  Head >= SuppElem, !, roman_empire(Tail, SuppElem, BigList, SmallList).
+roman_empire([Head|Tail], SuppElem, BigList, [Head|SmallList]):-
+  roman_empire(Tail, SuppElem, BigList, SmallList).
+  
+qsort([], []).
+qsort([Elem], [Elem]).
+qsort([SuppElem|Tail], ListSort):-
+  roman_empire(Tail, SuppElem, BigList, SmallList),
+  qsort(BigList, BigListSort),
+  qsort(SmallList, SmallListSort),!,
+  append(SmallListSort, [SuppElem|BigListSort], ListSort).
+  
+roman_empire_tree(L, Left, Right) :-
+	length(L, Length),
+	Middle is div(Length, 2),
+	length(Left, Middle),
+	append(Left, Right, L).
+	
+balanced_tree_continue([], empty).
+balanced_tree_continue(L, instant(R, LSubtree, RSubtree)) :-
+	roman_empire_tree(L, Left, [R|Right]),
+	balanced_tree_continue(Left, LSubtree),
+	balanced_tree_continue(Right, RSubtree).
+  
+balanced_tree(L, T) :-
+	qsort(L, SortL),
+	balanced_tree_continue(SortL, T).
