80d80eecdf0afb29fad4ff89f4a8c327aa7d7274
DLoseva <dariayal@yandex.ru>
2020-11-23 00:34:59 +0300

    Lab 3-5 Prolog
    Add resolutions labs 3-5.

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,16 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([], []).
+qsort([List], [List]).
+qsort([Head|List_tail], ListSort):-
+  list_split(List_tail, Head, Smaller, Biggest),
+  qsort(Smaller, SmallerSort),
+  qsort(Biggest, BiggestSort),!,
+  append(SmallerSort, [Head|BiggestSort], ListSort).
+  
+  
+list_split([], Z, [], []):-!.
+list_split([Head|List_tail], Z, [Head|Smaller], Biggest):-
+  Head < Z, list_split(List_tail, Z, Smaller, Biggest).
+list_split([Head|List_tail], Z, Smaller, [Head|Biggest]):-
+  Head >=Z, list_split(List_tail, Z, Smaller, Biggest).
--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,21 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult('3_quicksort.pl').
+
+balanced_tree(L, T) :-
+	qsort(L, List_sort),
+	create_tree(List_sort, T).
+	
+create_tree([], empty).
+create_tree(List_sort, instant(Root, L, R)) :-
+	split_tree(List_sort, List_sort_small, [Root|List_sort_big]),
+	create_tree(List_sort_small, L),
+	create_tree(List_sort_big, R).
+	
+split_tree(L, Left, Right) :-
+	length(L, Len),
+	M is div(Len, 2),
+	length(Left, M),
+	append(Left, Right, L).
