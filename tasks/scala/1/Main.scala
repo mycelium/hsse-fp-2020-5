bad57b2358c7b7484cadb1d01bcb6eef940af299
Angelina Barkanova <71984597+gelichan@users.noreply.github.com>
2020-11-24 20:30:26 +0300

    Added solution for Scala task 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -9,22 +9,43 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+	
+	println("\nParentheses Balancing")
+	println("Is ((())(()))()) balanced? " + balance("((())(()))())".toList))
+	println("Is (()((()))) balanced? " + balance("(()((())))".toList))
+	
+	println("\nCounting Change")
+    val money = 5
+    val coins = List(1,2,5,10)
+	print("Number of ways to have change for " + money + " using " + coins + ": ")
+    println(countChange(money,coins))
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-
+  def pascal(column: Int, row: Int): Int = {
+    if ((column == 0) || (column == row)) 1
+	else pascal(column - 1,row - 1) + pascal(column,row - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    @tailrec
+    def countingBalance(chars: List[Char], dif: Int): Boolean = {
+      if (chars.isEmpty) dif == 0
+      else if (dif < 0) false
+      else chars.head match {
+        case '(' => countingBalance(chars.tail, dif + 1)
+        case ')' => countingBalance(chars.tail, dif - 1)
+        case _ => countingBalance(chars.tail, dif)
+      }
+    }
+    countingBalance(chars,0)
   }
-
+  
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
@@ -33,6 +54,33 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+   countingWays(money,coins)
+ }
+  def countingWays(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || money - coins.head < 0 || coins.isEmpty) 0
+    else if (money - coins.head == 0) 1
+    else countChange(money - coins.head,coins) + countChange(money,coins.tail)
   }
 }
+
+  /**
+   * Pascal's Triangle
+   * 1
+   * 1 1
+   * 1 2 1
+   * 1 3 3 1
+   * 1 4 6 4 1
+   * 1 5 10 10 5 1
+   * 1 6 15 20 15 6 1
+   * 1 7 21 35 35 21 7 1
+   * 1 8 28 56 70 56 28 8 1
+   * 1 9 36 84 126 126 84 36 9 1
+   * 1 10 45 120 210 252 210 120 45 10 1
+   * 
+   * Parentheses Balancing
+   * Is ((())(()))()) balanced? false
+   * Is (()((()))) balanced? true
+   * 
+   * Counting Change
+   * Number of ways to have change for 5 using List(1, 2, 5, 10): 4
+   */
