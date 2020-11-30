13bd4909b2be9af09dcc0721a94e69c404e0eaca
Maxim Shkividorov <32208852+juseppeinside@users.noreply.github.com>
2020-11-30 20:40:23 +0000

    add haskel lab

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,8 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance [] [] = Just 0
+distance (x_param:xs) (y_param:ys) = 
+   if length xs /= length ys then Nothing else Just (assistant (x_param:xs) (y_param:ys)) where assistant [] [] = 
+   0 helper (x_param:xs) (y_param:ys) = 
+   if x_param /= y_param then (+) 1 ((assistant) xs ys) else (assistant) xs ys
