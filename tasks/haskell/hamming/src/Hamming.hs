5a118a5ecc17f15e8f987283c6918c41c4a7618e
Vladkuz <vladkuz100@mail.ru>
2020-11-30 23:46:40 +0300

    haskell lab implemented by VK
    Haskell lab implemented by VK

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,13 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+-- case diff length - impossible
+-- case same length - starter value 0
+distance xs ys  | (length xs) /= (length ys) = Nothing | otherwise = dist 0 xs ys                
+  where
+    dist mistakesequence [] [] = Just mistakesequence
+    dist mistakesequence (x:xs) (y:ys) = 
+-- compare
+      if x /= y
+        then dist (mistakesequence+1) xs ys
+      else dist mistakesequence xs ys
