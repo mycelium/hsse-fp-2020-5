0c9a06dc8d73fc2df63f6645a7efbb3d82d54351
Aleksandra Panarina <apanarina@b2broker.net>
2020-12-02 22:56:30 +0300

    Laba 1 Scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,33 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println("Balancing:")
+    println(balance("12(2)()".toList))
+    println("Count change:")
+    val l = List(4,8,9,12,15,16)
+    println(countChange(11, l))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || с == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+   if (chars.isEmpty)
+     count == 0
+   else if (chars.head == '(')
+     balance(chars.tail, count + 1)
+   else if (chars.head == ')' && count > 0)
+     balance(chars.tail, count - 1)
+   else
+     balance(chars.tail, count)
   }
 
   /**
@@ -33,6 +46,9 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (coins.isEmpty || (money - coins.head < 0)) 0
+        else if (money - coins.head == 0) 1
+        else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+ 
   }
 }
