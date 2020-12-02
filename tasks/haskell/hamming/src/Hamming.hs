2c659953a9a0c6f32356db62ff219a2572de1fba
Igor Konev <Igor.Konev@emc.com>
2020-12-03 00:55:45 +0300

    [HASKELL-1] Add Haskell lab

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,12 @@
 module Hamming (distance) where
 
+import Data.List
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = case length xs /= length ys of
+                      True -> Nothing
+                      _    -> Just (countElems 0 (filter (\x -> fst x /= snd x) (zip xs ys)))
+                 where
+                      countElems n [] = n
+                      countElems n (x:xs) = countElems (n+1) xs
+
