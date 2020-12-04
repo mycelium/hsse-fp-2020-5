32b0822a409db441293cba892adc5250331ebf30
Aleksandra Panarina <apanarina@b2broker.net>
2020-12-04 15:23:19 +0300

    Added scala lab2 and Haskell

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,13 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+--если строки имеют разную длину, то мы ничего не возращаем, если же все хорошо отсчет расстояния начинаем с 0
+distance xs ys  | (length xs) /= (length ys) = Nothing | otherwise = dist 0 xs ys
+  where
+    dist counter [] [] = Just counter
+--берем элемент из каждой строки
+    dist counter (x:xs) (y:ys) = 
+--проверяем элементы на равенство
+      if x /= y
+        then dist (counter+1) xs ys
+      else dist counter xs ys
