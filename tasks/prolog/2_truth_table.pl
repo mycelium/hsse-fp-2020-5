32601237a0df275c0073898b23f023d044391bb2
Vlad Sukhanov <vlad.sukhanov98@mail.ru>
2020-12-05 17:26:38 +0300

    Implemented 2nd lab

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,37 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+xor(A, B) :- A \= B.
+equ(A, B) :- A = B.
+not(A) :- \+ A.
+
+truth_value(true).
+truth_value(false).
+
+% Таблица истинности для бинарного предиката
+truth_table(A, B, Expression) :- write("A\tB\tResult"), nl, truth_value(A), truth_value(B), 
+								write(A), write('\t'), write(B), write('\t'), (Expression -> write(true); write(false)), nl, false.
+								
+% Таблица истиности для унарного предиката								
+truth_table(A, Expression) :- write("A\tResult"), nl, truth_value(A), write(A), write('\t'), (Expression -> write(true); write(false)), nl, false.
+
+% ?- truth_table(A, B, and(A, B)).
+% A       B       Result
+% true    true    true
+% true    false   false
+% false   true    false
+% false   false   false
+% false.
+
+% ?- truth_table(A, B, or(A, B)).
+% A       B       Result
+% true    true    true
+% true    false   true
+% false   true    true
+% false   false   false
+% false.
+
+etc...
