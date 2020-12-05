32ba7a321f4c78a03d6be49f3278e41523b0f875
Vlad Sukhanov <vlad.sukhanov98@mail.ru>
2020-12-05 19:50:24 +0300

    Implemented lab3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,28 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+% Для пустого списка сортировка не требуется
+qsort([],[]).
+
+% Быстрая сортировка
+% Pivot - стержень (опорный элемент)
+% Rest - остаток списка
+% SortedList - Отсортированыый список
+% Left, Right - вспомогательные элементы
+qsort([Pivot | Rest], SortedList) :-
+    append(LeftSorted, [Pivot | RightSorted], SortedList),
+    partition(Pivot, Rest, Left, Right),
+    qsort(Left, LeftSorted),
+    qsort(Right, RightSorted).
+
+% Разбиения
+partition(_, [], [], []).
+partition(Pivot, [Number | Rest], [Number | Left], Right) :-
+    Number =< Pivot,
+    partition(Pivot, Rest, Left, Right).
+partition(Pivot, [Number | Rest], Left, [Number |Right]) :-
+    Number > Pivot,
+    partition(Pivot, Rest, Left, Right).
+	
+% Example
+% ?- qsort([5,4,2,7,3,1], X).
+% X = [1, 2, 3, 4, 5, 7] .
