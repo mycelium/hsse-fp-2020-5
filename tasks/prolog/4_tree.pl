55b8664c24ac4f552a84698ff7072ac742d71b36
Daniil_Ageev <unselts@gmail.com>
2020-12-05 17:25:10 +0300

    prolog labs

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,14 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+:- consult('3_quicksort.pl').
+
+balanced_tree(List,T) :- qsort(List,Sorted),branching(Sorted,T).
+
+branching([],empty).
+branching(List,instant(Root,LTree,RTree)) :- splitting(List,L,[Root|R]),branching(L,LTree),branching(R,RTree).
+
+splitting(List,L,R) :- length(List,Length),Center is Length//2,length(L,Center),append(L,R,List).
+
+% ?- balanced_tree([1,5,2,6,3,8],K).
+% K = instant(5, instant(2, instant(1, empty, empty), instant(3, empty, empty)), instant(8, instant(6, empty, empty), empty)) 
