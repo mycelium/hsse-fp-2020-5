41f87afb16f666a0ae8099ca4dc79f5eb951be9c
Bubulgumus <Bubulgumus@users.noreply.github.com>
2020-12-05 13:46:41 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,45 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println()
+    print("balance for (abv)")
+    println(balance("(abv)".toList))
+    print("balance for ()()()())))")
+    println(balance("(()()()())))".toList))
+    print("coins for 157 and 1 2 5 10 coins ")
+    println(countChange(157 ,List(1, 2, 5, 10)))
+    
   }
 
   /**
    * Exercise 1
    */
+  
   def pascal(c: Int, r: Int): Int = {
 
+   if ( r == 0 || c == 0) { return 1 } 
+
+   pascal(c - 1, r - 1) + pascal(c, r - 1)
+      
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+   var count: Int = 0;
+    for ( i <- 0 to chars.length-1){
+         
+     if(chars(i).equals('(')){
+      count += 1; 
+     } 
+     else if(chars(i).equals(')')) {
+        count -= 1;
+     }
+    
+    }
+    if(count != 0) false
+    else true
   }
 
   /**
@@ -33,6 +58,18 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def variables(money: Int, coins: List[Int]): Int = {
+      if (coins.isEmpty) {
+        0
+      } else if (money - coins.head == 0) {
+        1
+      } else if (money - coins.head < 0) {
+        0
+      } else {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      }
+    }
 
+    variables(money, coins.sorted)
   }
 }
