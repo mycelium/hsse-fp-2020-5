a8cfd5fd63d699414b9db2f9f44f84e43cf7288c
MSI\echernaya <echernaya22@yandex.ru>
2020-12-06 22:20:53 +0300

    add lab on haskell

---
+++ Labs/haskell/hamming/src/Hamming.hs
@@ -0,0 +1,8 @@
+module Hamming (distance) where
+
+distance :: String -> String -> Maybe Int
+distance (x:xs) (y:ys) | x /= y = fmap (+1) tl
+                              | otherwise = tl
+    where tl = distance xs ys
+distance [] [] = Just 0
+distance _ _ = Nothing
