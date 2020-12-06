d6f38470e1cb711148114548daae32c7785bc7e8
evgrig <zhenya-rigin@mail.ru>
2020-12-06 17:58:12 +0300

    Prolog Lab1

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -5,13 +5,68 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+father(a,b).  % 1
+father(a,c).  % 2
+father(b,d).  % 3
+father(b,e).  % 4
+father(c,f).  % 5
+
+brother(X,Y) :- father(Fa, X), father(Fa, Y), X \= Y.
+
+cousin(X,Y) :- father(Fa1, X), father(Fa2, Y), brother(Fa1, Fa2).
+
+grandson(X,Y) :- father(Y, T), father(T, X).
+
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(T,X), descendent(T,Y).
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%?- brother(X,Y).
+%X = b,
+%Y = c ;
+%X = c,
+%Y = b ;
+%X = d,
+%Y = e ;
+%X = e,
+%Y = d ;
+%false.
+
+%?- cousin(X,Y).
+%X = d,
+%Y = f ;
+%X = e,
+%Y = f ;
+%X = f,
+%Y = d ;
+%X = f,
+%Y = e ;
+%false.
+
+%?- grandson(X,Y).
+%X = d,
+%Y = a ;
+%X = e,
+%Y = a ;
+%X = f,
+%Y = a ;
+%false.
+
+%?- descendent(X,Y).
+%X = b,
+%Y = a ;
+%X = c,
+%Y = a ;
+%X = d,
+%Y = b ;
+%X = e,
+%Y = b ;
+%X = f,
+%Y = c ;
+%X = d,
+%Y = a ;
+%X = e,
+%Y = a ;
+%X = f,
+%Y = a ;
+%false.
