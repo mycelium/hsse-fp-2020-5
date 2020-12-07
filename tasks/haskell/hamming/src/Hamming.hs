41a903b66a96b3851d2dd26931c18342996e13f8
agaev.aslan <agaev.aslan@gmail.com>
2020-12-07 23:19:17 +0300

    add haskell lab

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,4 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = if (length xs /= length ys) then Nothing else (Just (length (filter (\el -> (fst el) /= (snd el)) (zip xs ys))))
