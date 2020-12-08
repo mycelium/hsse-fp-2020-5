949f4ef7712070548992990499703858fbe5e377
evgrig <zhenya-rigin@mail.ru>
2020-12-08 08:11:31 +0300

    Lab 3 prolog

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+split([], Pivot, [], []):-!.
+split([Head|Tail], Pivot, Bigger, [Head|Smaller]):-
+  Head < Pivot, !, split(Tail, Pivot, Bigger, Smaller).
+split([Head|Tail], Pivot, [Head|Bigger], Smaller):-
+  split(Tail, Pivot, Bigger, Smaller).
+  
+qsort([], []).
+qsort([E], [E]).
+qsort([Pivot|Tail], ListSorted):-
+  split(Tail, Pivot, Bigger, Smaller),
+  qsort(Smaller, SmallerSorted),
+  qsort(Bigger, BiggerSorted),!,
+  append(SmallerSorted, [Pivot|BiggerSorted], ListSorted).
