972ffaf9acdff2e75d9ca02080178c007e9b9b20
Bubulgumus <Bubulgumus@users.noreply.github.com>
2020-12-08 07:56:02 +0300

    Update Hamming.hs

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,10 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = mistake xs ys 0
+
+mistake :: String -> String -> Int -> Maybe Int
+mistake [] [] n = Just n
+mistake xs [] n = Nothing
+mistake [] ys n = Nothing
+mistake (x:xs) (y:ys) n | (x /= y) = mistake xs ys (n + 1) | otherwise = mistake xs ys n
