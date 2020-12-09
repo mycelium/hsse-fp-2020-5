94174e856e91c849af0e599dd32c6dbcae5665c9
evgrig <zhenya-rigin@mail.ru>
2020-12-09 20:56:31 +0300

    Lab 1 Scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -15,14 +14,29 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
-
+  
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def balance_ad(chars: List[Char], n: Int): Boolean = {
+      if (chars==Nil)
+      {
+        if (n==0) true
+        else false
+      }
+      else if (chars=='('::chars.tail) balance_ad(chars.tail, n + 1)
+      else if (chars==')'::chars.tail)
+      {
+         if (n<1) false
+         else balance_ad(chars.tail, n - 1) 
+      }
+      else balance_ad(chars.tail, n)
+    }
+    balance_ad(chars, 0)
   }
 
   /**
@@ -33,6 +47,9 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money==0) 1
+    else if (coins==Nil) 0
+    else if (money-coins.head<0) countChange(money, coins.tail)
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail) 
   }
 }
