8f77e3ee004e56cbedba3a7b28d2a2add544e3e7
sabdam <sab.damirova@gmail.com>
2020-12-09 18:57:22 +0300

    Add lab 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([], []).
+qsort([L|T], K) :- split(L, T, Less, Great), qsort(Less, LessSort), qsort(Great, GreatSort), 							   append(LessSort, [L|GreatSort], K). 
+
+split(_, [], [], []).
+split(L, [Y|T], [Y|Less], Great) :- Y < L , split(L, T, Less, Great).
+split(L, [Y|T], Less, [Y|Great]) :- Y >= L, split(L, T, Less, Great).
+
+% Результаты:
+%
+% ?- qsort([37, 5, 0, -7], X).
+% X = [-7, 0, 5, 37] .
+%
+% ?- qsort([50, -8], X).
+% X = [-8, 50] .
+
