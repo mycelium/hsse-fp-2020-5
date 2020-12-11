cc478db41544e48ef41096a09e61f4dd83928eb3
sabdam <sab.damirova@gmail.com>
2020-12-12 01:42:45 +0300

    Add lab 2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,11 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(union(singletonSet(1), Set(1, 2, 3)))
+  printSet(intersect(singletonSet(1), Set(1, 2, 3)))
+  printSet(diff(Set(1, 2, 3), singletonSet(1)))
+  printSet(filter(Set(1, 2, 3), singletonSet(2)))
+  println(forall(Set(1, 2, 3), Set(1, 2, 3)))
+  println(exists(Set(1, 2, 3), Set(1, 2, 3)))
+  printSet(map(Set(1, 2, -3), math.abs))
 }
