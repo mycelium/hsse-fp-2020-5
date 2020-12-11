49a8f588dacd192a532d3a1e8dfe4bdddc611af3
Aleksandra Panarina <apanarina@b2broker.net>
2020-12-11 13:53:41 +0300

    Added function map and exists

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -68,12 +68,12 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set = y => exists(s, x => y == f(x))
   
   /**
    * Displays the contents of a set
