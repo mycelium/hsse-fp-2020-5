6e5823bc42492f969f7ecbc9760ca5f8c82083c7
sabdam <sab.damirova@gmail.com>
2020-12-12 00:58:41 +0300

    Add lab 2

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,3 @@
-package recfun
-import common._
-
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -11,18 +8,38 @@ object Main {
     }
   }
 
+    println()
+    print("balance (()(()) is ")
+    println(balance("(()(())".toList))  //false
+
+    print("balance (([()()](()))) is ")
+    println(balance("(([()()](())))".toList))  //true
+
+    print("number of ways to get 4 with 1 and 2 is ")
+    println(countChange(4, List(1, 2)))  //3
+    
+    print("number of ways to get 5 with 2 and 3 is ") 
+    println(countChange(5, List(2, 3)))  //1
+    
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+     if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+   def balanced(chars: List[Char], r: Int): Boolean = {
+      if (chars.isEmpty) r == 0
+      else if ("({[".contains(chars.head)) balanced(chars.tail, r + 1)
+      else if (")}]".contains(chars.head)) r > 0 && balanced(chars.tail, r - 1)
+      else balanced(chars.tail, r)
+    }
+    balanced(chars, 0)
   }
 
   /**
@@ -33,6 +50,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+   def count(m: Int, c: List[Int]): Int = {
+      if (c.isEmpty) 0
+      else if (m - c.head <  0) 0
+      else if (m - c.head == 0) 1
+      else countChange(m - c.head, c) + countChange(m, c.tail)
+    }
+    count(money, coins.sorted)
   }
+  
 }
