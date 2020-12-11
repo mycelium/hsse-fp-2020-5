17e084b15cda4eda5950558bd7e8a9cf07b0c3cb
Dmitry Ivanov <thormodred@gmail.com>
2020-12-11 04:15:50 +0300

    Добавляет Lab #1-2 (Prolog)

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -5,13 +5,86 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+    
+father(a,b).                
+father(a,c).
+father(b,d).
+father(b,e).
+father(c,f).
+
+% 1. brother(X,Y): X и Y - братья
+brother(X,Y) :-
+    father(F, X),
+    father(F, Y),
+    X \= Y.
+    
+% 2. cousin(X,Y): X и Y - двоюродные братья
+cousin(X, Y) :-
+    father(GDad, F1),
+    father(GDad, F2),
+    father(F1, X),
+    father(F2, Y),
+    F1 \= F2,
+    X \= Y.
+    
+% 3. grandson(X, Y): X - внук Y
+grandson(X, Y) :-
+    father(Y, F),
+    father(F, X),
+    X \= Y.
+
+% 4. descendent(X, Y): X - потомок Y
+descendent(X, Y) :-
+    father(Y, X).
+    
+descendent(X, Y) :-
+    father(Y, Z),
+    descendent(X, Z),
+	X \= Y.
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+% ?- brother(X,Y).
+% X = b,
+% Y = c
+% X = c,
+% Y = b
+% X = d,
+% Y = e
+% X = e,
+% Y = d
+
+% ?- cousin(X,Y).
+% X = d,
+% Y = f
+% X = e,
+% Y = f
+% X = f,
+% Y = d
+% X = f,
+% Y = e
+
+% ?- grandson(X,Y).
+% X = d, внук
+% Y = a  дедушка
+% X = e, внук
+% Y = a  дедушка
+% X = f, внук
+% Y = a  дедушка
+
+% ?- descendent(X,Y).
+% X = b, потомок
+% Y = a  предок
+% X = c, потомок
+% Y = a  предок
+% X = d, потомок
+% Y = b  предок
+% X = e, потомок
+% Y = b  предок
+% X = f, потомок
+% Y = c  предок
+% X = d, потомок
+% Y = a  предок
+% X = e, потомок
+% Y = a  предок
+% X = f, потомок
+% Y = a  предок
--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,41 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B) :- A, B.
+
+or(A, B) :- A; B.
+
+xor(A, B) :- not(equ(A, B)).
+
+not(A) :- \+ A.
+
+equ(A, B) :- A = B.
+
+
+expr(Expression, true) :- Expression, !.
+expr(Expression, fail).
+
+truth_table(A, B, Expression) :-
+	bool(A), 
+	bool(B),
+	expr(Expression, Res),
+	write(A), write('\t'),  write(B), write('\t'), write(Res), nl, 
+    fail.
+
+% пример из задания:
+% ?- truth_table(A,B,and(A,or(A,B))).
+% true    true    true
+% true    false   true
+% false   true    false
+% false   false   false
+
+% свой пример:
+% ?- truth_table(A, B, not(or(A, or(A, not(B))))).
+% true    true    false
+% true    false   false
+% false   true    true
+% false   false   false
