531a30902cc97ec0ab842926c77a15282ca6fbef
Dmitry Ivanov <thormodred@gmail.com>
2020-12-12 01:37:41 +0300

    Добавляет Lab 1-2 (Scala)

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,3 @@
-package recfun
-import common._
-
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -9,20 +6,37 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println()
+    println(balance("(())))))".toList))
+    println(balance("(((())))".toList))
+      
+    println()
+    println(countChange(33, List(3, 10)))
+    println(countChange(33, List(3, 5, 10)))  
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+      if (c == 0 || c == r) {
+          return 1
+      }
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+      def count(c: List[Char], counter: Int): Boolean = c match {
+          case Nil => counter == 0
+          case '(' :: tail => count(tail, counter + 1)
+          case ')' :: tail if counter < 1 => false
+          case ')' :: tail => count(tail, counter - 1)
+          case _ :: tail => count(tail, counter)
+      }
+      count(chars, 0)
   }
 
   /**
@@ -32,7 +46,19 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
+    
   def countChange(money: Int, coins: List[Int]): Int = {
-
+      def count(money: Int, coins: List[Int]): Int = {
+          if (coins.isEmpty) {
+            0
+          } else if (money - coins.head == 0) {
+            1
+          } else if (money - coins.head < 0) {
+            0
+          } else {
+            countChange(money - coins.head, coins) + countChange(money, coins.tail)
+          }
+      }
+      count(money, coins.sorted)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,31 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+    
+  val first_set: Set = it => 0 to 10 contains it
+  println("First set:")
+  printSet(first_set)
+
+  val second_set: Set = it => 5 to 20 contains it
+  println("Second set:")
+  printSet(second_set)
+
+  println("Their union:")
+  printSet(union(first_set, second_set))
+  println("First set elements that are not in the second one:")
+  printSet(diff(first_set, second_set))
+  println("Second set elements that are not in the first one:")
+  printSet(diff(second_set, first_set))
+  println("Intersection of two sets:")
+  printSet(intersect(first_set, second_set))
+
+  println("Apply x2 to each element of the first set:")
+  printSet(map(first_set, it => it * 2))
+  println("Apply x3 to each element of the second set:")
+  printSet(map(second_set, it => it * 3))
+
+  println("First set elements that are less than 5:")
+  printSet(filter(first_set, it => it < 5))
+  println("Second set elements that are greater than 10:")
+  printSet(filter(second_set, it => it > 10))
 }
