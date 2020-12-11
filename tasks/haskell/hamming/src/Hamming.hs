590d3c0f517e2d1909052b2a8dc31d9ba8818326
AlexandraSokolova <37152165+AlexandraSokolova@users.noreply.github.com>
2020-12-11 21:36:43 +0300

    Added Haskell lab

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = if (length xs == length ys) then Just (length(createDifferent(zip xs ys)))
+                 else Nothing
+
+createDifferent :: [(Char, Char)] -> [(Char, Char)]
+createDifferent [] = []
+createDifferent ((a,b) : xs)
+    | a /= b = (a,b) : (createDifferent xs)
+    | otherwise = createDifferent xs
