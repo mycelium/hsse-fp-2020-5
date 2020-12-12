2ceded1dea3faab13eecbe8992f4d17884ebd900
Alexander Aphonin <alex15051997@yandex.ru>
2020-12-12 22:01:12 +0300

    [lab 5] merge mock

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,11 @@
-$ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
+% определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
+
+mrg([], [], []).
+mrg(L1, [], L1).
+mrg([], L2, L2).
+
+mrg([LeftListHead | LeftListTail], [RightListHead | RightListTail], Res) :- 
+  LeftListHead < RightListHead -> append([LeftListHead], Res, Res), mrg(LeftListTail, [RightListHead | RightListTail], Res);
+  append([RightListHead], Res, Res), mrg([LeftListHead | LeftListTail], RightListTail, Res).
+
