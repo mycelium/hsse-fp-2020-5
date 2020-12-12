7902ce7efa5ae145d53ac53380f7ab88b6a49e4f
AleCopter <dollar0643@gmail.com>
2020-12-13 02:04:31 +0300

    Добавил Hamming.hs

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,7 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance :: String -> String -> Maybe Int
+distance xs ys = if ((length xs) == (length ys))
+    then Just (sum (map fromEnum (zipWith(/=) xs ys)))
+    else Nothing
