750f80d82ff8bd8ee1ce20d541c95c5ad9675a84
Alexander Aphonin <alex15051997@yandex.ru>
2020-12-12 22:15:11 +0300

    [lab 5] fix "if" condition
    add examples

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -5,7 +5,17 @@ mrg([], [], []).
 mrg(L1, [], L1).
 mrg([], L2, L2).
 
-mrg([LeftListHead | LeftListTail], [RightListHead | RightListTail], Res) :- 
-  LeftListHead < RightListHead -> append([LeftListHead], Res, Res), mrg(LeftListTail, [RightListHead | RightListTail], Res);
-  append([RightListHead], Res, Res), mrg([LeftListHead | LeftListTail], RightListTail, Res).
+% weirdly enough <= / >= clauses are not allowed in if else. So "if; if else; else" is used instead adding weird condition for 
+% ... equal elements. But works.
 
+mrg([LeftListHead | LeftListTail], [RightListHead | RightListTail], Res) :- 
+  LeftListHead < RightListHead -> append([LeftListHead], TempRes, Res), mrg(LeftListTail, [RightListHead | RightListTail], TempRes);
+  LeftListHead > RightListHead -> append([RightListHead], TempRes, Res), mrg([LeftListHead | LeftListTail], RightListTail, TempRes);
+    append([LeftListHead | [RightListHead]], TempRes, Res), mrg(LeftListTail, RightListTail, TempRes).
+
+
+% mrg([1, 2, 3], [4,5,6], R).
+% R = [1, 2, 3, 4, 5, 6]
+
+% mrg([1, 1, 3], [2,2,3], R).
+% R = [1, 1, 2, 2, 3, 3]
