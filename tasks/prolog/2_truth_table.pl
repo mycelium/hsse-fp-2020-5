e831881113a8fd187e50598bc6ad77d029d18a0c
AleCopter <dollar0643@gmail.com>
2020-12-12 12:02:49 +0300

    Добавил 2_truth_table.pl

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,49 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+% Придикаты 
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+equ(A,B) :- A = B.
+xor(A,B) :- A \= B.
+not(A) :- \+ A.
+
+ev(E, true) :- E, !.
+ev(_, false).
+
+bool(true).
+bool(false).
+
+truth_table(A, B, E) :-
+    bool(A),
+    bool(B),
+    write(A),
+    write('.\t '),
+    write(B),
+    write('.\t '),
+    ev(E, Result),
+    write(Result),
+    write('.\n '),
+    fail.
+
+% Проверка
+
+% truth_table(A,B,and(A,or(A,B))).
+% true. true. true.
+% true. fail. true.
+% fail. true. fail.
+% fail. fail. fail
+
+% truth_table(A,B,xor(not(A), or(A,B))).
+% true.   true.   true.
+% true.   false.  true.
+% false.  true.   true.
+% false.  false.  true
+
+% truth_table(A,B,or(A, not(B))).
+% true.   true.   true.
+% true.   false.  true.
+% false.  true.   false.
+% false.  false.  true.
