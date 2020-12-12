58a7df45f7851052be2399a9dd299529b6931a20
dadlex <alex15051997@yandex.ru>
2020-12-13 02:57:02 +0300

    [lab 6] add countDistance
    No empty case handling

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,7 +1,13 @@
 module Hamming (distance) where
 
+-- | zipWith is very convenient to do list comparison. So turning String into list.
 distance :: String -> String -> Maybe Int
 distance xs ys =  
     if length xs /= length ys then Nothing
-    else Just 0
+    else countDistance xs ys 0
 
+-- | no pre-increment. this isn't functional
+countDistance:: String -> String -> Int -> Maybe Int
+countDistance (x:xs) (y:ys) dist = 
+    if (x /= y) then countDistance xs ys (dist + 1)
+    else countDistance xs ys dist
