b9730650056dac594f58b3cf01316542b95d932e
VaultdVellerDao <71140850+VaultdVellerDao@users.noreply.github.com>
2020-12-13 01:55:53 +0300

    Update 4_tree.pl

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,22 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- consult('3_quicksort.pl').
+
+balanced_tree(L, T) :-
+	qsort(L, Sorted),
+	tree(Sorted, T).
+	
+tree([], empty).
+
+tree(L, instant(Root, LeftSub, RightSub)) :-
+	split_tree(L, Left, [R|Right]),
+	tree(Left, LeftSub),
+	tree(Right, RightSub).
+
+split_tree(L, Left, Right) :-
+	length(L, Length),
+	Middle is div(Length, 2),
+	length(Left, Middle),
+	append(Left, Right, L).
