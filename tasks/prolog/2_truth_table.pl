291e42757033af5554c64d84cbac11f36b672aa7
agaev.aslan <agaev.aslan@gmail.com>
2020-12-13 22:18:04 +0300

    add lab2 prolog

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,44 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+% ?- truth_table(A,B,xor(A,and(A,B))).
+% true true fail
+% true fail true
+% fail true fail
+% fail fail fail
+% 
+?- truth_table(A,B,not(xor(A,B))).
+% true true true
+% true fail fail
+% fail true fail
+% fail fail true
+% 
+
+bool(true).
+bool(false).
+
+and(A, B) :- A, B.
+or(A,B) :- A; B.
+not(A) :- \+ A.
+xor(A,B) :- and(or(A, B), not(and(A, B))).
+equ(A,B) :- not(xor(A,B)).
+
+value(true).
+value(fail).
+
+expr(E, true) :- E, !.
+expr(E, fail).
+
+truth_table(A,B,Expression) :-
+  value(A),
+  value(B),
+  write(A),
+  write(' '),
+  write(B),
+  write(' '),
+  expr(Expression, Res),
+  write(Res),
+  nl, 
+  fail.
+
