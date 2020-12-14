59b85cfefa678ddd2f3f09d6bc6ad355bdd5f53e
BRONZTRING <67222191+BRONZTRING@users.noreply.github.com>
2020-12-14 23:49:45 +0300

    Add files via upload

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,23 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def indexPar(chars: List[Char], parBalance: Int): Int = {
+      if (chars.isEmpty || parBalance < 0) parBalance
+      else if (chars.head == ')') indexPar(chars.tail, parBalance - 1)
+      else if (chars.head == '(') indexPar(chars.tail, parBalance + 1)
+      else indexPar(chars.tail, parBalance)
+    }
+               
+    indexPar(expressionChars, 0) == 0
   }
 
   /**
@@ -33,6 +42,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    def count(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money > 0 && coins.nonEmpty) count(money - coins.head, coins) + count(money, coins.tail)
+      else 0
+    }
+    count(money, coins)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -20,31 +20,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set = returnElem => elem == returnElem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = returnSet => s(returnSet) || t(returnSet)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = returnSet => s(returnSet) & t(returnSet)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = returnSet => s(returnSet) & !t(returnSet)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = returnSubset => s(returnSubset) & p(returnSubset)
   
 
   /**
@@ -57,23 +57,29 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if ( !s(a) | !p(a) ) false
+      else if (bound < a) true
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = 
+    {
+    !forall(s, k => !p(k))
+     }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set = 
+     {
+        apply => exists(s, el => f(el) == apply)
+     }
   
   /**
    * Displays the contents of a set
