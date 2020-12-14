970d3cbd3605ecde99c9587e38ce73df6661a251
regel <roman.egel@firstlinesoftware.com>
2020-12-14 23:06:25 +0300

    added 1,2 scala labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,55 @@
 package recfun
 import common._
 
-object Main {
+
+object First {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
-      for (col <- 0 to row)
+      for (_ <- 0 to 10 - row) {
+        print(" ")
+      }
+
+      for (col <- 0 to row) {
         print(pascal(col, row) + " ")
+      }
       println()
     }
+
+    println("\n\nBALANCE : ")
+    println(balance(List('d', '(', 'f',')', 'd', '(', 'f', '(', 'd', ')', 'l', ')')))
+    println(balance(List('d', ')', 'f',')', 'd', '(', 'f', '(', 'd', ')', 'l', ')')))
+
+    println("\n\nCountChange : ")
+
+    print(countChange(5, List(2, 3, 1, 4)))
+    print(countChange(4, List(2, 3)))
   }
+  
+  // 5*1, 3*1 + 2, 2*1 + 3, 1 + 4, 1 + 2 + 2, 2 + 3
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == r || c == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def walkRecursive(chars: List[Char]): Int = {
+        chars match {
+          case Nil => 0
+          case '('::tail => walkRecursive(tail) + 1
+          case ')'::tail => walkRecursive(tail) - 1
+          case _::tail => walkRecursive(tail)
+        }
+    }
+
+    walkRecursive(chars) == 0
   }
 
   /**
@@ -33,6 +60,10 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    (money, coins) match {
+      case (0, _) => 1
+      case (m, coins) if m < 0 || coins == Nil => 0
+      case (m, head::tail) => countChange(m - head, coins) + countChange(m, tail)
+    }
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,18 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(1), 1)) // true
+  println(contains(union(singletonSet(2), singletonSet(4)), 2)) // true
+  println(contains(union(singletonSet(2), singletonSet(4)), 3)) // false
+
+
+  println(contains(map(union(singletonSet(2), singletonSet(4)), v => v + 1), 5)) // true
+  println(contains(map(union(singletonSet(2), singletonSet(4)), v => v + 1), 3)) // true
+
+
+  println(forall(union(singletonSet(2), singletonSet(4)), v => v % 2 == 0)) // true
+  println(forall(union(singletonSet(2), singletonSet(4)), v => v % 3 == 0)) // false
+
+  println(exists(union(singletonSet(2), singletonSet(4)), v => v == 4)) // true
+  println(exists(union(singletonSet(2), singletonSet(4)), v => v == 5)) // false
 }
