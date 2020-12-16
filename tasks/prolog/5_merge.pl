7f26ba57273675d907ebda94a5f257aefebd03bd
wenxuezhen321 <71782717+wenxuezhen321@users.noreply.github.com>
2020-12-16 22:39:18 +0300

    Update 5_merge.pl

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,21 @@
-$ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
+% определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2
 % определяет список R, составленный из этих элементов
+
+mrg([], X, X).
+mrg(Y, [], Y).
+
+mrg([X|T_X], [Y|T_Y], [X|T_Merged]) :-
+	X =< Y,
+	mrg(T_X, [Y|T_Y], T_Merged).
+	
+mrg([X|T_X], [Y|T_Y], [Y|T_Merged]) :-
+	X > Y,
+	mrg([X|T_X], T_Y, T_Merged).
+
+mrg([X|T_X], [], [X|T_Merged]):- 
+	mrg(T_X, [], T_Merged).
+
+mrg([], [Y|T_Y], [Y|T_Merged]):- 
+	mrg([], T_Y, T_Merged).
+
+mrg([], [], []).
