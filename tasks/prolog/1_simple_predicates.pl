235362efa3e66b59f76afe9c061c805b9def41da
wenxuezhen321 <71782717+wenxuezhen321@users.noreply.github.com>
2020-12-16 12:18:39 +0300

    Update 1_simple_predicates.pl

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -11,7 +11,45 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+brother(X,Y) :- father(Z,X), father(Z,Y), dif(X,Y).
+cousin(X,Y) :- father(Z,X), father(Q,Y), dif(X,Y), brother(Z,Q).
+grandson(X,Y) :- father(Y,Z), father(Z,X).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,Z), descendent(X,Z).
+
+%?- brother(X,Y).
+%X = b, Y = c ;
+%X = c, Y = b ;
+%X = d, Y = e ;
+%X = e, Y = d ;
+%false.
+
+% ?- cousin(X,Y).
+% X = d, Y = f ;
+% X = e, Y = f ;
+% X = f, Y = d ;
+% X = f, Y = e ;
+% false.
+
+% ?- grandson(X,Y).
+% X = d, Y = a ;
+% X = e,  Y = a ;
+% X = f,  Y = a ;
+% false.
+
+% ?- grandson(X,Y).
+% X = d, Y = a ;
+% X = e, Y = a ;
+% X = f, Y = a ;
+% false.
+
+% ?- descendent(X,Y).
+% X = b, Y = a ;
+% X = c, Y = a ;
+% X = d, Y = b ;
+% X = e, Y = b ;
+% X = f, Y = c ;
+% X = d, Y = a ;
+% X = e, Y = a ;
+% X = f, Y = a ;
+% false.
