4c280d95b58674ec9a4c99c3492883694ac88a1f
Aleksei Zaichenkov <Aleksei_Zaichenkov@epam.com>
2020-12-16 05:09:15 +0300

    Added laboratory 1 for Scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,35 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(c: List[Char], counter: Int): Boolean = c match {
+      case Nil =>
+        counter == 0
+
+      case '(' ::
+        tail => count(tail, counter + 1)
+
+      case ')' ::
+        tail
+        if counter < 1 =>
+          false
+
+      case ')' ::
+        tail => count(tail, counter - 1)
+
+      case _ ::
+        tail => count(tail, counter)
+    }
+    count(chars, 0)
   }
 
   /**
@@ -33,6 +54,17 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    def makeCount(money_left: Int, coins_left: List[Int]): Int = {
+      if (coins_left.isEmpty) {
+        return 0
+      } else if (money_left - coins_left.head == 0){
+        return 1
+      } else if (money_left - coins_left.head < 0){
+        return 0
+      } else {
+        return countChange(money_left - coins_left.head, coins_left) + countChange(money_left, coins_left.tail)
+      }
+    }
+    makeCount(money, coins.sorted)
   }
 }
