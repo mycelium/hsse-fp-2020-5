cb25fe8d4f857fd6d212b68ae5b22a870fd9a9f7
Maxim Solomonov <solomonov.ms@edu.spbstu.ru>
2020-12-16 15:13:37 +0300

    Added lab 6 (Scala)

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
-package recfun
-import common._
+
 
 object Main {
   def main(args: Array[String]) {
@@ -9,30 +8,55 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("Balance parentheses")
+    println(balance("(a(bc(de)f)g(h)i)jk".toList))
+
+    println(countChange(100, List(1, 2, 5, 10, 50, 100)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (c == r)) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
-   * Exercise 2 Parentheses Balancing
+   * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(char: Char) : Int = chars.count(c => c == char)
+    def checkParentheses(right: Int, left: Int ) : Boolean = {
+      if (right == left) {
+        return true
+      }
+      false
+    }
+    if ((chars.contains('(') || chars.contains(')')) && checkParentheses(count('('), count(')'))) {
+      balance(chars.filter(c => c != '(' && c != ')'))
+    }
+    true
   }
 
   /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
+   * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money < 0) {
+      return 0
+    }
+    if (money == 0) {
+      return 1
+    }
+    coins match {
+      case head::tail => countChange(money - head, coins) + countChange(money, tail)
+      case Nil => 0
+    }
   }
+
 }
