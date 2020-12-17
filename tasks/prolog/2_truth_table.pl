6985521daf4e1b8cdb8efcb88363ba3b908945bc
NikitaCartes <nikich98@yandex.ru>
2020-12-17 04:48:37 +0300

    Second Labs

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -4,9 +4,35 @@
 	% xor(A, B)
 	% not(A)
 	% equ(A,B)
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+not(A) :- \+ A.
+xor(A, B) :- or(and(A, not(B)), and(not(A), B)).
+equ(A, B) :- not(xor(A, B)).
+
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
-% truth_table(A,B,and(A,or(A,B))).
-% true true true
-% true fail true
-% fail true fail
-% fail fail fail
+
+bool(true).
+bool(false).
+
+eval(Expression, true) :- Expression, !.
+eval(Expression, false) :- not(Expression), !.
+
+truth_table(A, B, Expression) :- bool(A), bool(B), eval(Expression, Result), 
+	write(A), write(" | "), write(B), write(" | "), write(Result), nl, fail.
+
+% Sheffer stroke
+% ?- truth_table(A,B,or(not(A),not(B))).
+% true | true | false
+% true | false | true
+% false | true | true
+% false | false | true
+% false.
+
+% Logical NOR
+% ?- truth_table(A,B,not(or(A, B))).
+% true | true | false
+% true | false | false
+% false | true | false
+% false | false | true
