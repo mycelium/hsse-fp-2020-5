aee8dd498eec39d07f620a6d707c16004b474734
kirmilwaukee <33043696+kirmilwaukee@users.noreply.github.com>
2020-12-17 15:27:10 +0300

    prolog added

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -5,13 +5,25 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+father(a,b).  % 1                 
+father(a,c).  % 2
+father(b,d).  % 3
+father(b,e).  % 4
+father(c,f).  % 5
+
+brother(X,Y) :- father(A,X), father(A,Y), X \= Y.
+cousin(X,Y) :- father(B,X), father(A,Y), brother(B,A).
+grandson(X,Y) :- father(Y,A), father(A,X).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,A), descendent(X,A).
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%	?- brother(X,Y).    % 1
+%	?- cousin(X,Y).     % 2
+%	?- grandson(X,Y).   % 3 
+%	?- descendent(X,Y). % 4
+
+% L = [[b, c], [c, b], [d, e], [e, d]].                  % 1
+% L = [[d, f], [e, f], [f, d], [f, e]].                  % 2
+% L = [[d, a], [e, a], [f, a]].                          % 3
+% L = [[b,a],[c,a],[d,b],[e,b],[f,c],[d,a],[e,a],[f,a]]. % 4
