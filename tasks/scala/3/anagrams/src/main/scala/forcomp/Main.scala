0df455b381bee0a6756d05dbf1457b5bf9100d69
NikitaCartes <nikich98@yandex.ru>
2021-01-10 17:19:13 +0300

    Add 2&3 scala labs

---
+++ tasks/scala/3/anagrams/src/main/scala/forcomp/Main.scala
@@ -0,0 +1,10 @@
+package Main
+
+
+  object Main extends App {
+
+    import forcomp._
+
+    println(Anagrams.sentenceAnagrams(List("Real", "fun")))
+    println(Anagrams.sentenceOccurrencesMap(List("The", "eyes")) == Anagrams.sentenceOccurrencesMap(List("They", "see")))
+  }
