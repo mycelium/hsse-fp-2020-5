0b8f9bf93998dfabcfb2548b6049da97f3267734
denis <denis-cher1998@yandex.ru>
2021-02-03 17:33:35 +0300

    add lab2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -1,6 +1,26 @@
 package funsets
 
 object Main extends App {
-  import FunSets._
-  println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(1), 1)) // true
+  println(contains(union(singletonSet(2), singletonSet(4)), 2)) // true
+  println(contains(union(singletonSet(2), singletonSet(4)), 3)) // false
+
+
+  println(contains(map(union(singletonSet(2), singletonSet(4)), v => v + 1), 5)) // true
+  println(contains(map(union(singletonSet(2), singletonSet(4)), v => v + 1), 3)) // true
+
+  printSet(intersect(union(singletonSet(2), singletonSet(4)),
+    union(singletonSet(5), singletonSet(4)))) // {4}
+
+  printSet(union(union(singletonSet(2), singletonSet(4)),
+    union(singletonSet(5), singletonSet(4)))) // {2, 4, 5}
+
+
+  println(forall(union(singletonSet(2), singletonSet(4)), v => v % 2 == 0)) // true
+  println(forall(union(singletonSet(2), singletonSet(4)), v => v % 3 == 0)) // false
+
+  println(exists(union(singletonSet(2), singletonSet(4)), v => v == 4)) // true
+  println(exists(union(singletonSet(2), singletonSet(4)), v => v == 5)) // false
+
+  printSet(union(singletonSet(2), singletonSet(4)))
 }
